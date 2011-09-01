a3df07cd3aa75f8fba9055f137b0490e2b67a86f
yellowxi <yellowxi@gmail.com>
2011-09-01 21:50:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Praycard::Application.config.secret_token = 'fa31f979db2619ffad88a513f7cf759f9e131f28e82a77e8c12674d85bcbe2a7f602db6aa6a75ddd0f3f4683ad0946bfe735e7b2ea174cb8f1f5c80a226199b1'

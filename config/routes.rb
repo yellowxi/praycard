90e1ba93287a4d65beb9487b7df3393f9965f549
yellowxi <yellowxi@gmail.com>
2011-09-08 01:07:36 +0800

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Praycard::Application.routes.draw do
-  get "users/new"
-
+  
+  resources :users
+  
   get "pages/home"
-
   get "pages/contact"
 
   # The priority is based upon order of creation:

b2a3fb5ffb85780db6db816c0d8901225d67e7c5
yellowxi <yellowxi@gmail.com>
2011-09-17 22:34:02 +0800

    added some page layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Praycard::Application.routes.draw do
-  
   resources :users
   
-  get "pages/home"
-  get "pages/contact"
+  
+  match '/signup',  :to => 'users#new'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
